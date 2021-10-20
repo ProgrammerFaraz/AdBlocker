6d56280902e80b69f744567cfbad6b29c8cf9023
FarazAhmedKhan <farazahmedkhan18@gmail.com>
2021-10-21 01:17:55 +0500

    [new purchase view file added]

---
+++ Security Manager/Views/Purchase View/NewPurchaseView.swift
@@ -0,0 +1,22 @@
+//
+//  NewPurchaseView.swift
+//  Security Manager
+//
+//  Created by Faraz on 10/21/21.
+//
+
+import SwiftUI
+
+struct NewPurchaseView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct NewPurchaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewPurchaseView()
+    }
+}
